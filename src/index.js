b4bc98855ccafbd5eec55d267cd3ffd840eeff4e
Jarno <jarno.forsblom@student.lut.fi>
2022-09-29 19:07:18 +0300

    Initial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
-import "./styles.css";
+//import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const textShow = document.getElementById("input-show");
+const submitButton = document.getElementById("submit-data");
+const showTable = document.getElementById>("show-table");
+
+
+
+submitButton.addEventListener("click", async function(){
+  q = textShow.value;
+  textShow.value = "";
+  
+  getShowData(q);
+})
+
+async function getShowData(showName){
+  const url = "https://api.tvmaze.com/search/shows?q="+showName;
+  const showPromise = await fetch(url)
+  const showJSON = await showPromise.json()
+
+  showJSON.forEach(element => {
+
+    let divData = document.createElement("div");
+    divData.className = "show-data";
+
+    let divInfo = document.createElement("div");
+    divInfo.className = "show-info";
+
+    let head1 = document.createElement("h1");
+    head1.innerText = element.show.name;
+
+    let summary = document.createElement("p");
+    summary.innerText = element.show.summary.replace(/<p>|<\/p>/ig, "");
+
+    
+    if (element.show.image != null){
+      let image = document.createElement("img")
+      image.src = element.show.image.medium;
+      divData.append(image);
+    }
+    divInfo.appendChild(head1);
+    divInfo.appendChild(summary);
+    
+    divData.append(divInfo);
+    document.body.appendChild(divData);
+
+
+  });
+
+}
